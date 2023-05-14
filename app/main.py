a2c873083819b34fa3462db7bea9e7b21b405185
amitsgh <amitsingh1911nov@gmail.com>
2023-05-14 14:58:23 +0530

    update

--- app/main.py
+++ app/main.py
@@ -9,49 +9,150 @@ import utils
 
 
 # Set page title and favicon.
-st.set_page_config(
-    page_title="ML Code Generator",
+st.set_page_config(page_title="ML Code Generator")
+
+# Set up Github access for "Open in Colab" button.
+try:
+    load_dotenv()  # load environment variables from .env file
+    github_token = os.getenv("GITHUB_TOKEN")
+    repo_name = os.getenv("REPO_NAME")
+    if github_token and repo_name:
+        g = Github(github_token)
+        repo = g.get_repo(repo_name)
+        colab_enabled = True
+
+        def add_to_colab(notebook):
+            """
+            Adds notebook to Colab by pushing it to Github repo and returning Colab link.
+            """
+            notebook_id = str(uuid.uuid4())
+            repo.create_file(
+                f"notebooks/{notebook_id}/generated-notebook.ipynb",
+                f"Added notebook {notebook_id}",
+                notebook,
+            )
+            colab_link = f"http://colab.research.google.com/github/{repo_name}/blob/master/notebooks/{notebook_id}/generated-notebook.ipynb"
+            return colab_link
+    else:
+        colab_enabled = False
+except Exception as e:
+    colab_enabled = False
+    st.warning(f"Failed to set up Github access: {str(e)}")
+
+
+# Page title and introduction.
+st.title("Machine Learning Code Generator")
+st.markdown("<br>", unsafe_allow_html=True)
+st.markdown(
+    """
+    Generate your machine learning starter code in five simple steps. 
+
+    1. Select Task (Anomaly Detection or Classification or Clustering).
+     2. Select Algorithm
+     3. Specify data set and hyperparameters.
+     4. Starter code will be generated below.
+     5. Download the code.
+     """,
+    unsafe_allow_html=True,
+)
+st.markdown("<br>", unsafe_allow_html=True)
+
+
+# Find available templates and show selectors for task and framework in sidebar. 
+# These selectors determine which template is used (and also which template-specific
+# sidebar components are shown below).
+template_dir = "templates"
+template_dirs = defaultdict(dict)
+for dirpath, dirnames, filenames in os.walk(template_dir):
+    task = os.path.basename(dirpath)
+    if not task:
+        continue
+    for framework in dirnames:
+        if framework == "__pycache__":
+            continue
+        template_dirs[task][framework] = os.path.join(template_dir, task, framework)
+del template_dirs["templates"]
+
+with st.sidebar:
+    st.write("## Task")
+    task = st.selectbox(
+        "Which problem do you want to solve?", list(template_dirs.keys())
+    )
+    if isinstance(template_dirs[task], dict):
+        framework = st.selectbox(
+            "In which framework?", list(template_dirs[task].keys())
+        )
+        template_path = template_dirs[task][framework]
+    else:
+        template_path = template_dirs[task]
+
+
+# Show template-specific sidebar components. 
+template_sidebar = utils.import_from_file(
+    "sidebar", os.path.join(template_path, "sidebar.py")
 )
 
-# Set up github access for "Open in Colab" button.
-load_dotenv()  # load environment variables from .env file
-print(os.getenv('REPO_NAME'))
-if os.getenv("GITHUB_TOKEN") and os.getenv("REPO_NAME"):
-    g = Github(os.getenv("GITHUB_TOKEN"))
-    repo = g.get_repo(os.getenv("REPO_NAME"))
-    colab_enabled = True
-
-    def add_to_colab(notebook):
-        """Adds notebook to Colab by pushing it to Github repo and returning Colab link."""
-        notebook_id = str(uuid.uuid4())
-        repo.create_file(
-            f"notebooks/{notebook_id}/generated-notebook.ipynb",
-            f"Added notebook {notebook_id}",
-            notebook,
-        )
-        colab_link = f"http://colab.research.google.com/github/{os.getenv('REPO_NAME')}/blob/master/notebooks/{notebook_id}/generated-notebook.ipynb"
-        return colab_link
-else:
-    colab_enabled = False
+try:
+    inputs = template_sidebar.show()
+except Exception as e:
+    st.warning(f"Failed to load sidebar: {str(e)}")
+    inputs = {}
 
 
-# Page title.
-st.title("Machine Learning Code Generator")
+# Generate code and notebook based on template.
+env = Environment(
+    loader=FileSystemLoader(template_path),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
 
-# st.markdown("-----")
-st.markdown("<br>", unsafe_allow_html=True)
-"""
+try:
+    template = env.get_template("code-template.py.jinja")
+    code = template.render(header=utils.code_header, notebook=False, **inputs)
+    notebook_code = template.render(header=utils.notebook_header, notebook=True, **inputs)
+    notebook = utils.to_notebook(notebook_code)
+except Exception as e:
+    st.error(f"Error while generating code: {e}")
+    st.stop()
 
-Generate your machine learning starter code in five simple steps. 
 
-1. Select Task (Anomaly Detection or Classification or Clustering).
-2. Select Algorithm
-3. Specify data set and hyperparameters.
-4. Starter code will be generated below.
-5. Download the code.
-"""
+st.write("")  # add vertical space
+col1, col2, col3 = st.columns(3)
+open_colab = col1.button("🚀 Open in Colab")
 
-st.markdown("<br>", unsafe_allow_html=True)
+with col2:
+    try:
+        utils.download_button(code, "generated-code.py", "🐍 Download (.py)")
+    except Exception as e:
+        st.error(f"Error while downloading Python code: {e}")
+with col3:
+    try:
+        utils.download_button(notebook, "generated-notebook.ipynb", "📓 Download (.ipynb)")
+    except Exception as e:
+        st.error(f"Error while downloading Jupyter notebook: {e}")
+
+colab_error = st.empty()
+
+
+# Use try-except blocks to handle exceptions that might occur while displaying the code or opening the Colab link
+try:
+    st.code(code)
+    # Handle "Open Colab" button. Down here because to open the new web page, it
+    # needs to create a temporary element, which we don't want to show above.
+    if open_colab:
+        if colab_enabled:
+            colab_link = add_to_colab(notebook)
+            utils.open_link(colab_link)
+        else:
+            colab_error.error(
+                """
+                **Colab support is disabled.** (If you are hosting this: Create a Github 
+                repo to store notebooks and register it via a .env file)
+                """
+            )
+except Exception as e:
+    st.error(f"Error while displaying code or opening Colab: {e}")
+   
 
 # templates = {
 #     'Anomaly Detection': {
@@ -72,88 +173,3 @@ st.markdown("<br>", unsafe_allow_html=True)
 #         'OPTICS': 'templates/Clustering/OPTICS',
 #     }
 # }
-
-template_dir = 'templates'
-
-template_dirs = defaultdict(dict)
-
-for dirpath, dirnames, filenames in os.walk(template_dir):
-    task = os.path.basename(dirpath)
-    print(task)
-    if not task:
-        continue
-    for framework in dirnames:
-        if framework == '__pycache__':
-            continue
-        print(framework, '------')
-        template_dirs[task][framework] = os.path.join(template_dir, task, framework)
-
-del template_dirs['templates']
-
-import json
-print(json.dumps(template_dirs, indent=4))
-
-# Show selectors for task and framework in sidebar (based on template_dict). These
-# selectors determine which template (from template_dict) is used (and also which
-# template-specific sidebar components are shown below).
-with st.sidebar:
-    st.write("## Task")
-    task = st.selectbox(
-        "Which problem do you want to solve?", list(template_dirs.keys())
-    )
-    if isinstance(template_dirs[task], dict):
-        framework = st.selectbox(
-            "In which framework?", list(template_dirs[task].keys())
-        )
-        template_path = template_dirs[task][framework]
-    else:
-        template_path = template_dirs[task]
-
-# Show template-specific sidebar components (based on sidebar.py in the template dir).
-template_sidebar = utils.import_from_file(
-    "template_sidebar", os.path.join(template_path, "sidebar.py")
-)
-
-inputs = template_sidebar.show()
-
-
-print(template_path, '000000000000000000000000')
-# Generate code and notebook based on template.py.jinja file in the template dir.
-env = Environment(
-    loader=FileSystemLoader(template_path), 
-    trim_blocks=True, 
-    lstrip_blocks=True,
-)
-
-template = env.get_template("code-template.py.jinja")
-code = template.render(header=utils.code_header, notebook=False, **inputs)
-notebook_code = template.render(header=utils.notebook_header, notebook=True, **inputs)
-notebook = utils.to_notebook(notebook_code)
-
-
-st.write("")  # add vertical space
-col1, col2, col3 = st.columns(3)
-open_colab = col1.button("🚀 Open in Colab")  # logic handled further down
-with col2:
-    utils.download_button(code, "generated-code.py", "🐍 Download (.py)")
-with col3:
-    utils.download_button(notebook, "generated-notebook.ipynb", "📓 Download (.ipynb)")
-colab_error = st.empty()
-
-
-st.code(code)
-
-
-# Handle "Open Colab" button. Down here because to open the new web page, it
-# needs to create a temporary element, which we don't want to show above.
-if open_colab:
-    if colab_enabled:
-        colab_link = add_to_colab(notebook)
-        utils.open_link(colab_link)
-    else:
-        colab_error.error(
-            """
-            **Colab support is disabled.** (If you are hosting this: Create a Github 
-            repo to store notebooks and register it via a .env file)
-            """
-        )
--- app/utils.py
+++ app/utils.py
@@ -20,10 +20,17 @@ def import_from_file(module_name: str, filepath: str):
     Returns:
         The module
     """
-    spec = importlib.util.spec_from_file_location(module_name, filepath)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+    try:
+        # Create a module spec from the file path
+        spec = importlib.util.spec_from_file_location(module_name, filepath)
+        # Create a module from the spec
+        module = importlib.util.module_from_spec(spec)
+        # Execute the module's code
+        spec.loader.exec_module(module)
+        return module
+    except Exception as e:
+        # Handle any exceptions that may occur during module import
+        st.error(f"Error importing module {module_name}: {e}")
 
 
 def notebook_header(text):
@@ -49,32 +56,48 @@ def code_header(text):
 
 def to_notebook(code):
     """Converts Python code to Jupyter notebook format."""
-    notebook = jupytext.reads(code, fmt="py")
-    return jupytext.writes(notebook, fmt="ipynb")
+    try:
+        # Read the Python code as a notebook
+        notebook = jupytext.reads(code, fmt="py")
+        # Write the notebook as a .ipynb file
+        return jupytext.writes(notebook, fmt="ipynb")
+    except Exception as e:
+        # Handle any exceptions that may occur during notebook conversion
+        st.error(f"Error converting code to notebook: {e}")
 
 
 def open_link(url, new_tab=True):
     """Dirty hack to open a new web page with a streamlit button."""
-    # From: https://discuss.streamlit.io/t/how-to-link-a-button-to-a-webpage/1661/3
-    if new_tab:
-        js = f"window.open('{url}')"  # New tab or window
-    else:
-        js = f"window.location.href = '{url}'"  # Current tab
-    html = '<img src onerror="{}">'.format(js)
-    div = Div(text=html)
-    st.bokeh_chart(div)
+    try:
+        if new_tab:
+            # Open the URL in a new tab or window
+            js = f"window.open('{url}')"
+        else:
+            # Open the URL in the current tab
+            js = f"window.location.href = '{url}'"
+        # Create an HTML div with the JavaScript code to open the URL
+        html = '<img src onerror="{}">'.format(js)
+        div = Div(text=html)
+        # Display the HTML div with the Bokeh chart widget
+        st.bokeh_chart(div)
+    except Exception as e:
+        # Handle any exceptions that may occur during link opening
+        st.error(f"Error opening link: {e}")
 
 
 def download_button(object_to_download, download_filename, button_text):
     try:
-        # some strings <-> bytes conversions necessary here
+        # Try encoding object_to_download as a string and then encoding as base64
         b64 = base64.b64encode(object_to_download.encode()).decode()
-    except AttributeError as e:
+    except AttributeError:
+        # If object_to_download is already bytes, just encode as base64
         b64 = base64.b64encode(object_to_download).decode()
 
+    # Generate unique ID for button element
     button_uuid = str(uuid.uuid4()).replace("-", "")
     button_id = re.sub("\d+", "", button_uuid)
 
+    # Define custom CSS styles for button element
     custom_css = f""" 
         <style>
             #{button_id} {{
@@ -100,13 +123,14 @@ def download_button(object_to_download, download_filename, button_text):
                 box-shadow: none;
                 background-color: rgb(246, 51, 102);
                 color: white;
-                }}
+            }}
         </style> """
 
+    # Generate HTML for download link with button element
     dl_link = (
         custom_css
         + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br><br>'
     )
-    # dl_link = f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}"><input type="button" kind="primary" value="{button_text}"></a><br></br>'
 
+    # Render the download link with button element using Streamlit
     st.markdown(dl_link, unsafe_allow_html=True)
